7c0a75ce58bf84161a16f7d3a8f57c878f81c941
narrowtomato <aaronhahn6@gmail.com>
2020-09-25 09:45:43 -0500

    Add files via upload

---
+++ cname_modify.py
@@ -0,0 +1,49 @@
+import requests
+import base64
+import getpass
+
+#Get info
+ibserver = input("IB Server: ")
+wapiversion = input("WAPI Version: ")
+username = input("user: ")
+passwd = getpass.getpass("pass: ")
+changeto = input("CNAME Destination: ")
+
+data = username + ':' + passwd
+
+# Standard Base64 Encoding
+encodedBytes = base64.b64encode(data.encode("utf-8"))
+encodedStr = str(encodedBytes, "utf-8")
+
+urlsfile = open("urllist.txt")
+
+for name in urlsfile:
+    #URL that looks for the existing record
+    url = "https://" + ibserver + "/wapi/v" + wapiversion + "/record:cname?name=" + name.strip()
+
+    payload = {}
+    headers = {
+      'Authorization': 'Basic ' + encodedStr,
+    }
+
+    response = requests.request("GET", url, headers=headers, data = payload, verify=False)
+
+    print(response.json())
+
+    referenceurl = response.json()[0]['_ref']
+
+    #Make a new URL with _ref to use in the PUT request
+    url = "https://" + ibserver + "/wapi/v" + wapiversion + "/" + referenceurl
+
+    #Payload
+    payload = "{\n    \"canonical\" : \"" + changeto + "\"\n}"
+    headers = {
+      'Authorization': 'Basic ' + encodedStr,
+      'Content-Type': 'application/json',
+    }
+
+    response = requests.request("PUT", url, headers=headers, data = payload, verify=False)
+    
+    print(response.json())
+
+urlsfile.close()
